ec4db3dbfa9826a50be4aa1dcf4deded94634cea
Hadley <hadleys@uchicago.edu>
2019-09-11 18:40:31 -0500

    Correction means  for #1-3
    We were calculating means the wrong way, updated with correct means for 1-3

--- challenge_reproducibility.R
+++ challenge_reproducibility.R
@@ -22,26 +22,39 @@ load("pvalueData_PNAS.rda")
 
 
 # 1) Plot the Poisson distribution with the same mean as the spider counts, along with the data
-spider_count_mean <- arthropods$C_count_of_boards_with_k_spiders %>% mean()
- 
+
+#Calculate the mean number of total number of observed spiders
+total_observation_count_spiders <- sum(arthropods[ ,"C_count_of_boards_with_k_spiders"])
+spider_count_mean <- (sum(arthropods$k_number_of_arthropods*arthropods$C_count_of_boards_with_k_spiders))/total_observation_count_spiders
+
 rpois(n = 1000, lambda = spider_count_mean) %>% hist(main = paste("Histogram of" , "Spider Counts"),col="lightblue1", border="blue")
 
 spider_pois <- rpois(n = 1000, lambda = spider_count_mean)
 
+
+
 # 2) Plot the Poisson distribution with the same mean as the sowbug counts, along with the data
-sowbugs_count_mean <- arthropods$C_count_of_boards_with_k_sowbugs %>% mean()
+total_observation_count_sowbugs <- sum(arthropods[ ,"C_count_of_boards_with_k_sowbugs"])
+sowbugs_count_mean <- (sum(arthropods$k_number_of_arthropods*arthropods$C_count_of_boards_with_k_sowbugs))/total_observation_count_sowbugs
 
 rpois(n = 1000, lambda = sowbugs_count_mean) %>% hist(main = paste("Histogram of" , "Sowbugs Counts"), col="lightblue1", border="blue")
 
 sowbug_pois <- rpois(n = 1000, lambda = sowbugs_count_mean)
 
+
+
 # 3) Plot the Poisson distribution with the same mean as the weevil egg counts, along with the data
-weevil_count_mean <- weevil$C_count_of_beans_with_k_eggs %>% mean()
+total_observation_count_weevil <- sum(weevil[ ,"C_count_of_beans_with_k_eggs"])
+weevil_count_mean <- (sum(weevil$k_number_of_eggs*weevil$C_count_of_beans_with_k_eggs))/total_observation_count_weevil
 
 rpois(n = 1000, lambda = weevil_count_mean) %>% hist(main = paste("Histogram of" , "Weevil Eggs Counts"), col="lightblue1", border="blue")
 
+weevil_pois <- rpois(n = 1000, lambda = weevil_count_mean)
+
+
 
 # 4) Add a curve to Plot 1) showing the LGP distribution with the parameter hint below for the spider counts
+
 spider_pois %>%
   as_tibble() %>%
   ggplot(aes(x = value)) +
